1c246208a32618e596ba00425238c9bc7091f98a
jendlrs <jensennics.dlrs@gmail.com>
2021-12-06 18:26:02 +0800

    Get third number ranging 0-9

--- lottery.py
+++ lottery.py
@@ -15,16 +15,19 @@ def lottery():
     while True:
         luckynumbers = random.sample (range (0,9),3) #This will generate 3 winning numbers
         while True:
-            first_ = int (input ("Enter first number: "))
+            first_ = int (input ("Enter your first number: "))
             if first_ >9 or first_ <=-1:
                 print ("Make sure that your number is ranging 0 to 9.") 
                 continue #if the user enters a number that is greater than 9 or a negative number, it will ask for 1st number again.
             while True:
-                second_ = int (input("Enter second number: "))
+                second_ = int (input("Enter your second number: "))
                 if second_ >9 or second_ <=-1:
                     print ("Make sure that your number is ranging 0 to 9.")
-                else:
-                    return first_, second_
+                while True:
+                    third_ = int (input("Enter your third number: "))
+                    if third_ >9 or third_ <=1:
+                        print ("Make sure that your number is ranging 0 to 9.")
+                    else:
+                        return first_, second_, third_
 
-
-first, second_ = lottery()
+first, second, third = lottery()
