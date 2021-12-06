c6c389aeac4f46e88073961a0ddf28bda53e12fd
jendlrs <jensennics.dlrs@gmail.com>
2021-12-07 00:04:42 +0800

    Final code. I just added text colors.

--- guess_number.py
+++ guess_number.py
@@ -7,9 +7,9 @@
 import random
 import time
 
-print ("\nWelcome to Number Guessing Game")
+print ("\nWelcome to \033[33mNumber Guessing Game!\033[0m")
 name = input ("\nWhat is your name? ")
-print (f"\nHi {name}! This game will generate one mystery number that you will have to guess.")
+print (f"\nHi \033[36m{name}!\033[0m This game will generate one mystery number that you will have to guess.")
 time.sleep (0.5)
 print ("\nGenerating mystery number....")
 time.sleep (1.5)
@@ -22,18 +22,18 @@ def guess_number ():
         try:
             user_guess = int(input("\nWhat is your guess? "))
             if user_guess == number_:
-                print (f"\nCongratulations! You got it right. The mystery number is {number_}.\n")
+                print (f"\n\033[1;32;40mCongratulations!\033[0;37;40m You got it right. The mystery number is \033[32m{number_}.\033[0m\n")
                 break
             else:
                 if user_guess >100 or user_guess <=-1:
-                    print ("\nMake sure that you have entered a number ranging 0 to 99.")
+                    print ("\n\033[1mMake sure that you have entered a number ranging 0 to 99.\033[0m")
                     continue
                 elif user_guess > number_:
-                    print ("\nWRONG")
+                    print ("\n\033[91mWRONG\033[0m")
                     print ("Hint: Your input is GREATER THAN the mystery number. Try reducing it.")
                     continue
                 else:
-                    print ("\nWRONG")
+                    print ("\n\033[91mWRONG\033[0m")
                     print ("Hint: Your input is LESS THAN the mystery number. Try increasing it.")
                     continue
                 
