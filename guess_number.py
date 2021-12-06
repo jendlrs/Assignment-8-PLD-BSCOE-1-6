0f67cc585a6805ef63f62a1eea5c958cb1ca389e
jendlrs <jensennics.dlrs@gmail.com>
2021-12-06 23:13:46 +0800

    I added another while true loop to make sure that the user only enter a number included in the range 0 to 99.

--- guess_number.py
+++ guess_number.py
@@ -9,19 +9,31 @@ import random
 print ("\nWelcome to Number Guessing Game")
 name = input ("\nWhat is your name? ")
 print (f"Hi {name}! This game will generate one mystery number that you will have to guess.")
-
+print ("Generating mystery number....")
 def guess_number ():
     number_ = random.randrange(0,100)
     while True:
-        user_guess = int(input("\nWhat is your guess? "))
-        if user_guess == number_:
-            print (f"\nCongratulations! You got it right. The correct number is {number_}.")
-            break
-        elif user_guess > number_:
-            print ("\nWRONG")
-            print ("Hint: Your input is GREATER THAN the random number to be guess.")
-        else:
-            print ("\nWRONG")
-            print ("Hint: Your input is LESS THAN the number to be guess.")
+        try:
+            while True:
+                user_guess = int(input("\nWhat is your guess? "))
+                if user_guess >100 or user_guess <=-1:
+                    print ("Make sure that you have entered a number ranging 0 to 99.")
+                    continue
+                while True:
+                    if user_guess == number_:
+                        print (f"\nCongratulations! You got it right. The correct number is {number_}.\n")
+                        break
+                    elif user_guess > number_:
+                        print ("\nWRONG")
+                        print ("Hint: Your input is GREATER THAN the random number to be guess.")
+                        break
+                    else:
+                        print ("\nWRONG")
+                        print ("Hint: Your input is LESS THAN the number to be guess.")
+                        break
+                
+        except ValueError:
+            print ("This program only accepts number. Try again.")
+            continue
 
 guess_number()
