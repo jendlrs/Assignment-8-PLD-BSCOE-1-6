aa351502e99637655be8f30967df2279f65d7f73
jendlrs <jensennics.dlrs@gmail.com>
2021-12-06 18:29:23 +0800

    I used while true, try, except valueError. ValueError is for instances that user accidentally enter a letter or other characters.

--- lottery.py
+++ lottery.py
@@ -14,20 +14,28 @@ import random
 def lottery():
     while True:
         luckynumbers = random.sample (range (0,9),3) #This will generate 3 winning numbers
-        while True:
-            first_ = int (input ("Enter your first number: "))
-            if first_ >9 or first_ <=-1:
-                print ("Make sure that your number is ranging 0 to 9.") 
-                continue #if the user enters a number that is greater than 9 or a negative number, it will ask for 1st number again.
+        try:
             while True:
-                second_ = int (input("Enter your second number: "))
-                if second_ >9 or second_ <=-1:
-                    print ("Make sure that your number is ranging 0 to 9.")
+                first_ = int (input ("Enter your first number: "))
+                if first_ >9 or first_ <=-1:
+                    print ("Make sure that your number is ranging 0 to 9.") 
+                    continue #if the user enters a number that is greater than 9 or a negative number, it will ask for 1st number again.
                 while True:
-                    third_ = int (input("Enter your third number: "))
-                    if third_ >9 or third_ <=1:
+                    second_ = int (input("Enter your second number: "))
+                    if second_ >9 or second_ <=-1:
                         print ("Make sure that your number is ranging 0 to 9.")
-                    else:
-                        return first_, second_, third_
+                    while True:
+                        third_ = int (input("Enter your third number: "))
+                        if third_ >9 or third_ <=1:
+                            print ("Make sure that your number is ranging 0 to 9.")
+                        else:
+                            return first_, second_, third_
+        except ValueError:
+            print ("Invalid Input! Make sure to enter numbers only.")
+            ready = input ("Do you want to try again? y/n ")
+            if ready == 'y':
+                continue
+            else:
+                print ("Thank you for playing Jensen's lottery.")
 
 first, second, third = lottery()
