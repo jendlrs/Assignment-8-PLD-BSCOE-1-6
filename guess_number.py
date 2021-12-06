c32b8d8d8bb487f81c75aaa247689a90fdbb5a0c
jendlrs <jensennics.dlrs@gmail.com>
2021-12-06 19:34:01 +0800

    Generate one number ranging 0-100.

---
+++ guess_number.py
@@ -0,0 +1,13 @@
+#Program 2: Guess the number
+#Generate 1 random number (0-100)
+#Ask the user to guess the number
+#Display “Greater than” if the inputed number is greater than the random number
+#Display “Less than” if the inputed number is less than the random number
+#Repeat asking the user until the random number has been guessed correctly.
+import random
+
+def guess_number ():
+    number = random.randrange(0,100)
+    print (number)
+
+guess_number()
