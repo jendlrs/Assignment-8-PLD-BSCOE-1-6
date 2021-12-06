2e6ff3d8d0bac2dde30fafd4cda3317dcdf62ca4
jendlrs <jensennics.dlrs@gmail.com>
2021-12-06 23:44:55 +0800

    I used time.sleep in time module to display that the program is generating a number.

--- guess_number.py
+++ guess_number.py
@@ -5,32 +5,37 @@
 #Display “Less than” if the inputed number is less than the random number
 #Repeat asking the user until the random number has been guessed correctly.
 import random
+import time
 
 print ("\nWelcome to Number Guessing Game")
 name = input ("\nWhat is your name? ")
-print (f"Hi {name}! This game will generate one mystery number that you will have to guess.")
-print ("Generating mystery number....")
+print (f"\nHi {name}! This game will generate one mystery number that you will have to guess.")
+time.sleep (0.5)
+print ("\nGenerating mystery number....")
+time.sleep (1.5)
+print ("\nThe mystery number is ready.")
+time.sleep (1)
+
 def guess_number ():
     number_ = random.randrange(0,100)
     while True:
         try:
-            while True:
-                user_guess = int(input("\nWhat is your guess? "))
+            user_guess = int(input("\nWhat is your guess? "))
+            if user_guess == number_:
+                print (f"\nCongratulations! You got it right. The mystery number is {number_}.\n")
+                break
+            else:
                 if user_guess >100 or user_guess <=-1:
-                    print ("Make sure that you have entered a number ranging 0 to 99.")
+                    print ("\nMake sure that you have entered a number ranging 0 to 99.")
                     continue
-                while True:
-                    if user_guess == number_:
-                        print (f"\nCongratulations! You got it right. The correct number is {number_}.\n")
-                        break
-                    elif user_guess > number_:
-                        print ("\nWRONG")
-                        print ("Hint: Your input is GREATER THAN the random number to be guess.")
-                        break
-                    else:
-                        print ("\nWRONG")
-                        print ("Hint: Your input is LESS THAN the number to be guess.")
-                        break
+                elif user_guess > number_:
+                    print ("\nWRONG")
+                    print ("Hint: Your input is GREATER THAN the mystery number. Try reducing it.")
+                    continue
+                else:
+                    print ("\nWRONG")
+                    print ("Hint: Your input is LESS THAN the mystery number. Try increasing it.")
+                    continue
                 
         except ValueError:
             print ("This program only accepts number. Try again.")
